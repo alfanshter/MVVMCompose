7196bbe6a2ba7b1cb63109b8245413998349bf9e
alfanshter <alfanno8@gmail.com>
2024-03-17 18:30:22 +0700

    navigation with Nav Arrgument

--- app/src/main/java/com/example/mvvmcompose/MainActivity.kt
+++ app/src/main/java/com/example/mvvmcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.mvvmcompose.api.TweetApi
 import com.example.mvvmcompose.screen.CategoryScreen
 import com.example.mvvmcompose.screen.DetailScreen
@@ -21,17 +28,55 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MVVMComposeTheme {
-                // A surface container using the 'background' color from the theme
-                DetailScreen()
-            }
+            App()
         }
     }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "registration"  ){
+        composable(route = "registration"){
+            RegistrationScreen{
+                navController.navigate("main/{$it}")
+            }
+        }
+        composable(route = "login"){
+            LoginScreen()
+        }
+        composable(route = "main/{email}", arguments = listOf(
+            navArgument("email"){
+                type = NavType.StringType
+            }
+        )){
+            val email = it.arguments!!.getString("email")
+            MainScreen(email!!)
+        }
+    }
+}
+
+@Composable
+fun RegistrationScreen(onClick : (email : String) -> Unit) {
+        Text(text = "Registration", style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.clickable {
+                onClick("alfanno8@gmail.com")
+            })
+}
+
+@Composable
+fun LoginScreen() {
+    Text(text = "login", style = MaterialTheme.typography.headlineSmall)
+}
+
+@Composable
+fun MainScreen(email : String) {
+    Text(text = "Main Screen - $email", style = MaterialTheme.typography.headlineSmall)
 }
